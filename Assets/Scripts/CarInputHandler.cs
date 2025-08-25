99f52ad82581dc9d8af1db2acdadc48b2a9a0d65
Kushagra-0 <kushgarashukla.dev@gmail.com>
2025-08-26 00:21:57 +0530

    initialized and started working on car game

---
+++ Assets/Scripts/CarInputHandler.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CarInputHandler : MonoBehaviour
+{
+    private InputSystem_Actions inputActions;
+    private Vector2 moveInput;
+
+    void Awake()
+    {
+        inputActions = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    void Update()
+    {
+        moveInput = inputActions.Player.Move.ReadValue<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    public Vector2 GetMoveInput()
+    {
+        return moveInput;
+    }
+}
